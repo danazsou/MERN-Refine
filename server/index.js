695950887c8c6063cd11bbd3d86251ef33d8b39a
danazou <dana.sou@gmail.com>
2023-02-21 13:26:22 -0800

    starting back end with expres dependencies

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', (req, res) =>{
+res.send ({message: 'Hey World!'});
+})
+
+const startServer = async () => {
+    try{
+        //connect to server
+    } catch(error) {
+        console.log(error);
+    }
+}
