f57253275ac86d6ab5de33af4b8833028447f969
danazou <dana.sou@gmail.com>
2023-02-24 17:19:31 -0800

    added route files

--- server/mongodb/models/user.js
+++ server/mongodb/models/user.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    avatar: {typ: String, reguired: true},
+    avatar: {type: String, required: true},
     allProperties: [{type: mongoose.Schema.Types.ObjectId, red: 'Property'}],
 });
 
-const user.Model = mongoose.nodel('User', UserSchema);
+const userModel = mongoose.model('User', UserSchema);
 
 export default userModel;
