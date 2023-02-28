06b8b5d5ed671b53461bd9ab42b9770fe3bf3c7a
danazou <dana.sou@gmail.com>
2023-02-27 19:42:14 -0800

    property card complete and working

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -1,26 +1,74 @@
 import Property from '../mongodb/models/property.js';
 import User from '../mongodb/models/user.js';
 
+import mongoose from 'mongoose'
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
-const getAllProperties = async (req, res) => {};
-const getPropertyDetail = async (req, res) => {};
+dotenv.config();
 
-const createProperty = async (req,res) => {
-    const { title, description, propertyType, location, price, photo,
-    email} = req.body;
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 
-    //start a new session
-    const session = await mongoose.startSession();
-    session.startTransaction();
+})
+
+const getAllProperties = async (req, res) => {
+    try{
+       const properties = await Property.find({}).limit(req.query._end);
+
+       res.status(200).json(properties);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
 };
 
-    const user = await User.findOne({email}).session(session);
 
-    if (!user) throw new Error('User not found');
+const getPropertyDetail = async (req, res) => {};
 
-    //const photoUrl = ...
+const createProperty = async (req, res) => {
+    try {
+        const {
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo,
+            email,
+        } = req.body;
+
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        const user = await User.findOne({ email }).session(session);
+
+        if (!user) throw new Error("User not found");
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newProperty = await Property.create({
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo: photoUrl.url,
+            creator: user._id,
+        });
+
+        user.allProperties.push(newProperty._id);
+        await user.save({ session });
+
+        await session.commitTransaction();
+
+        res.status(200).json({ message: "Property created successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 
 const updateProperty = async(req, res) => {};
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = (url) => {
-    mongoose.set('strictQuery', true);
+    mongoose.set("strictQuery", true);
 
-    mongoose.connect(url)
-        .then(() => console.log('MongoDB connected'))
+    mongoose
+        .connect(url)
+        .then(() => console.log("MongoDB connected"))
         .catch((error) => console.log(error));
 };
 
